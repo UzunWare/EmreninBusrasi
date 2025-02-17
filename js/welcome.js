e0c90e223d6eef7e571ab806959b5c9837fc3fb6
Emre Korkmaz <emrek@Mac.attlocal.net>
2025-02-17 00:05:45 -0600

    v4

---
+++ js/welcome.js
@@ -0,0 +1,31 @@
+
+
+function timeElapse(date){
+	var current = Date();
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var days = Math.floor(seconds / (3600 * 24));
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600);
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	seconds = seconds % 3600;
+	var minutes = Math.floor(seconds / 60);
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	seconds = seconds % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
+	$("#elapseClock").html(result);
+}
+
+// Sayfa geçişi ve müzik kontrolü
+document.getElementById("enterButton").addEventListener("click", function () {
+    document.getElementById("bgMusic").play(); // Müziği başlat
+    setTimeout(() => {
+        window.location.href = "/pages/intro.html"; // `intro.html`'e yönlendir
+    }, 500); // Geçişi pürüzsüz yap
+});
